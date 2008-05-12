eb3454f1e3dccfdc2b232388eb43502d7417765d
Shawn Moore <sartak@bestpractical.com>
2008-05-12 13:29:26 +0000

    Bump to 0.03

--- lib/Date/Extract.pm
+++ lib/Date/Extract.pm
@@ -18,11 +18,11 @@ Date::Extract - extract probable dates from strings
 
 =head1 VERSION
 
-Version 0.02 released 13 Feb 08
+Version 0.03 released 12 May 08
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
